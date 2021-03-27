f0d4d9cf55fe886b36bc6c87ed0cb3aaa4af0ece
Pablo Klaschka <pklaschka@users.noreply.github.com>
2021-03-27 01:42:49 +0100

    feat(frontend): Add Favicons (#4)
    Closes: #2

--- index.php
+++ index.php
@@ -19,6 +19,20 @@ require_once 'system/setup.php';
     <meta name="robots" content="noindex,nofollow">
     <title>WüSpace Login</title>
     <link href="css/tailwind.css" rel="stylesheet">
+
+    <!--region Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/favicons/site.webmanifest">
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000">
+    <link rel="shortcut icon" href="/favicons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="W&uuml;Space Login">
+    <meta name="application-name" content="W&uuml;Space Login">
+    <meta name="msapplication-TileColor" content="#aaaaaa">
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <!--endregion-->
 </head>
 <body class="flex justify-center min-h-screen bg-primary bg-gray-100 p-4">
 <div class="max-w-md m-auto p-4 shadow bg-white rounded-2xl font-body">
